af6633c086720dadfded6b23f0f0496161b823b0
MamadTvl <mtavallai80@gmail.com>
2023-09-07 12:28:34 +0330

    add env loader

---
+++ load_env.sh
@@ -0,0 +1,6 @@
+source_env() {
+  env=${1:-.env}
+  [ ! -f "${env}" ] && { echo "Env file ${env} doesn't exist"; return 1; }
+  eval $(sed -e '/^\s*$/d' -e '/^\s*#/d' -e 's/=/="/' -e 's/$/"/' -e 's/^/export /' "${env}")
+}
+source_env $1
